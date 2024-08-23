b939c9f750644006463a9225c8a849f645f59066
maq33 <145473642+rodrigorecchia@users.noreply.github.com>
2024-08-23 18:51:12 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://jvsc99.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://rodrigorecchia.github.io/preloaderJs/MAQ33LOGOPNG.png';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
